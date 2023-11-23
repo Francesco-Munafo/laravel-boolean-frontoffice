ac3e77f338f7a6673231cb45129c378ed36f6d99
lorenzo-neri <lorenzo21neri@gmail.com>
2023-11-23 15:51:07 +0100

    init vite+vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+
+  data() {
+    return {
+      message: 'Hello guys!'
+    }
+  },
+
+
+  methods: {},
+
+
+
+  mounted() {
+    console.log(`Hi!`)
+  }
+}
+</script>
+
+<template>
+  {{ message }}
+</template>
+
+<script lang="scss"></script>
