e2e5c336a34c1868df162fe4dd693f05dc452dd4
matteoNapoli42 <matteon4242@gmail.com>
2023-11-23 16:39:21 +0100

    add use bootstrap to scss, starting ContactView

--- src/view/ContactView.vue
+++ src/view/ContactView.vue
@@ -1,12 +1,74 @@
 <template>
-    <div>
-
+    <h1 class=" text-center">
+        Contacts
+    </h1>
+    <div class="container d-flex flex-wrap">
+        <div class="col-4 card">
+            <div class=" card-header">
+                <h2>Francesco Munafò</h2>
+            </div>
+            <div class=" card-body">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptas dolore id nostrum optio
+                    est ipsam nisi quas repellat unde repellendus, eius dolorum sapiente atque ut dolor commodi temporibus
+                    quidem.
+                </p>
+            </div>
+        </div>
+        <div class="col-4 card">
+            <div class=" card-header">
+                <h2>Lorenzo Neri</h2>
+            </div>
+            <div class=" card-body">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptas dolore id nostrum optio
+                    est ipsam nisi quas repellat unde repellendus, eius dolorum sapiente atque ut dolor commodi temporibus
+                    quidem.
+                </p>
+            </div>
+        </div>
+        <div class="col-4 card">
+            <div class=" card-header">
+                <h2>Giacono Branchi</h2>
+            </div>
+            <div class=" card-body">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptas dolore id nostrum optio
+                    est ipsam nisi quas repellat unde repellendus, eius dolorum sapiente atque ut dolor commodi temporibus
+                    quidem.
+                </p>
+            </div>
+        </div>
+        <div class="col-4 card">
+            <div class=" card-header">
+                <h2>Matteo Nocera</h2>
+            </div>
+            <div class=" card-body">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptas dolore id nostrum optio
+                    est ipsam nisi quas repellat unde repellendus, eius dolorum sapiente atque ut dolor commodi temporibus
+                    quidem.
+                </p>
+            </div>
+        </div>
+        <div class="col-4 card">
+            <div class=" card-header">
+                <h2>Matteo Napoli</h2>
+            </div>
+            <div class=" card-body">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos voluptas dolore id nostrum optio
+                    est ipsam nisi quas repellat unde repellendus, eius dolorum sapiente atque ut dolor commodi temporibus
+                    quidem.
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-
+    name: 'Contact'
 }
 </script>
 
