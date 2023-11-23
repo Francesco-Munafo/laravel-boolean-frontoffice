9b8a43f5e607a23d7504506ec337f59289891ea9
Francesco-Munafo <acquamarea1@gmail.com>
2023-11-23 16:15:14 +0100

    Import router and bs5 in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import '../src/assets/style/style.scss'
-import App from './App.vue'
+import { createApp } from "vue";
+import { router } from "./router.js";
+import "../src/assets/style/style.scss";
+import App from "./App.vue";
+import * as bootstrap from "bootstrap";
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount("#app");
